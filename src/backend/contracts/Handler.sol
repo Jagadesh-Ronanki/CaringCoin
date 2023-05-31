7fc3c4df98085608f729a6e56f7bd6a481af40f7
Jagadesh-Ronanki <jagadesh.ronanki@gmail.com>
2023-05-31 19:20:17 +0530

    renames

---
+++ src/backend/contracts/Handler.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IUserRegistry {
+    struct User {
+        uint256 level;
+        bool registered;
+        uint256 appreciationBalance;
+        uint256 contributionBalance;
+        uint256 appreciationsTaken;
+        uint256 appreciationsGiven;
+        uint256 takenAmt;
+        uint256 givenAmt;
+        uint256 tokenId;
+        bool tokenHolder;
+    }
+    function updateAppreciator(address appreciator, uint256 amt) external returns (bool);
+    function updateCreator(address creator, uint256) external returns (bool);
+    function withdraw(address creator, uint256 fee, uint256 withdrawalThresholdInEth) external;
+    function getUserDetails(address user) external view returns (User memory);
+}
+
+interface IPriceConversion {
+    function UsdtoEth(uint256) external returns (uint256);
+}
+
+interface IVariables {
+    function retriveBaseThreshold() external view returns (uint256);
+    function retrivePerWithdrawal() external view returns (uint256);
+}
+
+contract Handler is Ownable {
+    IUserRegistry userRegistry;
+    IPriceConversion priceConvertor;
+    IVariables variables;
+    // @TODO VARIABLE 3
+    // uint256 private baseThreshold = 3; // USD
+
+    event AddedFunds(address indexed sender, uint256 amount);
+
+    constructor() {
+    }
+
+    function setUserRegistry(address _userRegistryAddr) external onlyOwner {
+        userRegistry = IUserRegistry(_userRegistryAddr);
+    }
+    
+    function setPriceConversion(address _priceConversionAddr) external onlyOwner {
+        priceConvertor = IPriceConversion(_priceConversionAddr);
+    }
+
+    function setVariables(address _variables) external onlyOwner {
+        variables = IVariables(_variables);
+    }    
+    
+    function receiveAmount(address creator, address appreciator) external payable returns (bool) {
+        uint256 level = userRegistry.getUserDetails(creator).level;
+        uint256 maxAppreciation = (2**level - level);
+        uint256 maxAppreciationEth = priceConvertor.UsdtoEth(maxAppreciation);
+        require(msg.value <= maxAppreciationEth, "max appreciation amount exceeds");        
+        bool updatedCreator = userRegistry.updateCreator(creator, msg.value);    
+        require(updatedCreator, "Failed to update creator");
+        bool updatedAppreciator = userRegistry.updateAppreciator(appreciator, msg.value);    
+        require(updatedAppreciator, "Failed to update creator");
+
+        return true;
+    }
+
+    function withdraw(address creator) external payable returns (bool) {
+        IUserRegistry.User memory user = userRegistry.getUserDetails(creator);
+        uint256 level = user.level;
+        uint256 appreciationBalance = user.appreciationBalance;
+        uint256 withdrawalThreshold = calculateWithdrawalThreshold(level);
+        uint256 withdrawalThresholdInEth = priceConvertor.UsdtoEth(withdrawalThreshold);
+        require(withdrawalThresholdInEth <= appreciationBalance, "Withdrawal threshold not met");
+        // @TODO Variable perWithdrawal 10
+        uint256 perWithdrawal = variables.retrivePerWithdrawal();
+        uint256 fee = withdrawalThresholdInEth * perWithdrawal / 100;
+        userRegistry.withdraw(creator, fee, withdrawalThresholdInEth);
+        uint256 amt = withdrawalThresholdInEth - fee;
+        (bool success, ) = payable(creator).call{value: amt}("");
+        require(success, "Withdrawal failed");
+        return success;
+    }
+
+    function calculateWithdrawalThreshold(uint256 level) internal view returns (uint256) {
+        uint256 _threshold = variables.retriveBaseThreshold();
+        // @TODO Variable perWithdrawal 10
+        uint256 perWithdrawal = variables.retrivePerWithdrawal();
+        for (uint256 i = 2; i <= level; i++) {
+            uint256 percentageIncrease = _threshold * perWithdrawal / 100;
+            _threshold += percentageIncrease;
+        }
+
+        return _threshold;
+    }
+
+    // @TODO remove these utilities add in Variables.sol
+    function getBaseThreshold() public view returns (uint256) {
+        return variables.retriveBaseThreshold();
+    }
+    
+    receive() external payable {
+        emit AddedFunds(msg.sender, msg.value);
+    }
+}
---
+++ src/backend/contracts/UserRegistry.sol
@@ -0,0 +1,139 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+interface IGovernanceToken {
+    function safeMint(address to) external returns (uint256); 
+}
+
+contract UserRegistry {
+
+    address owner;
+    address handler;
+    address governanceToken;
+
+    struct User {
+        /* string name; */
+        uint256 level;
+        bool registered;
+        uint256 appreciationBalance;
+        uint256 contributionBalance;
+        uint256 appreciationsTaken;
+        uint256 appreciationsGiven;
+        uint256 takenAmt;
+        uint256 givenAmt;
+        uint256 tokenId;
+        bool tokenHolder;
+    }
+
+    mapping(address => User) public users;
+    event UserRegistered(address indexed id);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(string memory des) {
+        require(msg.sender == owner, des);
+        _;
+    }
+    
+    modifier onlyHandler(string memory des) {
+        require(msg.sender == handler, des);
+        _;
+    }
+    
+    modifier onlyGovernanceToken(string memory des) {
+        require(msg.sender == governanceToken, des);
+        _;
+    }
+
+    function setHandler(address _handler) public onlyOwner("Only owner can update handler") {
+        handler = _handler;
+    }
+
+    function setGovernanceToken(address _tokenAddr) external onlyOwner("Only owner can update governanceTokenAddr") {
+        governanceToken = _tokenAddr;
+    }
+
+    function registerUser() external {
+        require(!users[msg.sender].registered, "User already registered");
+        users[msg.sender] = User(
+           /*  generateRandomUsername(), */
+            1,
+            true,
+            0 wei,
+            0 wei,
+            0,
+            0,
+            0 wei,
+            0 wei,
+            0,
+            false
+        );
+        emit UserRegistered(msg.sender);
+    }
+
+    function addContributionBal(address userAddr) public payable returns (bool) {
+        require(users[userAddr].registered, "User not registered");
+        User storage user = users[userAddr];
+        user.contributionBalance = user.contributionBalance + msg.value;
+        return true;
+    }
+
+    function updateAppreciator(address appreciator, uint256 amt) external onlyHandler("Can't update") returns (bool){
+        User storage user = users[appreciator];
+        require(user.contributionBalance > amt, "insufficient contribution balance");
+        user.appreciationsGiven++;
+        user.givenAmt = user.givenAmt + amt;
+        user.contributionBalance = user.contributionBalance - amt;
+        return true;
+    }
+
+    function updateCreator(address creator, uint256 amt) external onlyHandler("Can't update") returns (bool) {
+        User storage user = users[creator];
+        user.appreciationBalance = user.appreciationBalance + amt;
+        user.appreciationsTaken++;
+        user.takenAmt = user.takenAmt + amt;
+        return true;
+    }
+
+    // restrict to be only called by handler
+    function withdraw(address creator, uint256 fee, uint256 withdrawalThresholdInEth) external onlyHandler("Can't withdraw") {
+        User storage user = users[creator];
+        user.level++;
+        user.appreciationBalance -= withdrawalThresholdInEth;
+        user.contributionBalance += fee;
+    }
+
+    function setTokenId(address _user, uint256 _tokenId) external onlyGovernanceToken("Only governance token can update tokenId") {
+        IERC721 nft = IERC721(governanceToken);
+        require(_user == nft.ownerOf(_tokenId), "Token owner can update details");
+        User storage user = users[_user];
+        user.tokenId = _tokenId;
+        user.tokenHolder = true;
+    }
+
+    function getUserDetails(address user) external view returns (User memory) {
+        return users[user];
+    }
+
+    function isRegistered(address _user) public view returns (bool) {
+        return users[_user].registered;
+    } 
+
+    function mintCaringToken() external {
+        IGovernanceToken tokenContract = IGovernanceToken(governanceToken); 
+        tokenContract.safeMint(msg.sender);
+    }
+
+    /* function generateRandomUsername() internal view returns (string memory) {
+        uint256 randomNumber = uint256(keccak256(abi.encodePacked(
+            block.timestamp,
+            msg.sender,
+            block.prevrandao
+        )));
+        return string(abi.encodePacked("User_", randomNumber));
+    } */
+}
